bd6606a47651952d472a1f815a497e88f89ec580
S2Hyeon <tmdgus908@naver.com>
2022-03-16 17:41:35 +0900

    소수 찾기2 (modified)

--- BOJ/BOJ_2581.py
+++ BOJ/BOJ_2581.py
@@ -10,7 +10,7 @@ for i in range(m, n + 1):
     cnt = 1
     if i < 2:
         continue
-    for j in range(2, int((i + 1) ** 0.5) + 1):
+    for j in range(2, int(i ** 0.5) + 1):
         if i % j == 0:
             cnt += 1
     if cnt == 1:
