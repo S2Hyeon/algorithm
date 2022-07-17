85c34bbf18acc9967526c7e81cbc179ae261c70d
SeungHyeon <58657060+S2Hyeon@users.noreply.github.com>
2022-07-17 23:05:01 +0900

    Delete Python directory

--- Python/BOJ/BOJ_10250.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-for i in range(t):
-    h, w, n = map(int, input().split())
-    yy = n % h
-    xx = n // h + 1
-    if yy == 0:
-        yy = h
-        xx -= 1
-    print(f'{yy}%02d' % xx)
--- Python/SWEA/SWEA_14178.py
+++
@@ -1,6 +0,0 @@
-import math
-T = int(input())
-for i in range(T):
-    N, D = map(int, input().split())
-    result = math.ceil(N / (2 * D + 1))
-    print("#{} {}".format(i + 1, result))
