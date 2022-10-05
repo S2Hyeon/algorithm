658c55bcb8756b0ba9349010c4c60504d3d89f2c
S2Hyeon <tmdgus908@naver.com>
2022-10-06 01:09:44 +0900

    치즈

--- Java/BOJ/BOJ_2636.java
+++ Java/BOJ/BOJ_2636.java
@@ -22,15 +22,6 @@ public class BOJ_2636 {
         }
     }
     public static void main(String[] args) throws IOException {
-        // 테두리부터 치즈가 없는 부분(흰색)을 bfs로 탐색해서
-        // 탐색중에 만나는 치즈 부분이 다음에 없어질 치즈의 테두리 => 큐에 저장
-        //     근데 시간효율성을 위해 반복할때마다 흰색 부분의 탐색 시작 위치를
-        //    없어질 치즈의 테두리 중 가장 위쪽에 있는 곳의 1열부터 하면 좋을 것 같다.
-        // 큐에서 poll 하면서 해당 위치를 흰색(0)으로 표시
-
-        // 치즈의 크기를 입력받으면서 저장(1의 개수 세기)
-        // 테두리에서 지울때마다 지운 개수 만큼 저장되어있던 치즈크기 빼고
-        // 만약 뺀 결과가 0 이라면 마지막 남은 개수 복원해서 출력
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
